0e9606c36dfde88dc16d2a5aa44c196abd4deec3
Горелько Никита <nikita.gorelko@mail.ru>
2023-05-02 01:54:20 +0300

    before review version

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -59,7 +59,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    },
 }
 
 AUTH_PASSWORD_VALIDATORS = [
@@ -97,8 +97,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-   'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
-   'AUTH_HEADER_TYPES': ('Bearer',),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -10,14 +10,14 @@ urlpatterns = [
     path(
         'redoc/',
         TemplateView.as_view(template_name='redoc.html'),
-        name='redoc'
+        name='redoc',
     ),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT,
     )
     urlpatterns += static(
-        settings.STATIC_URL, document_root=settings.STATIC_ROOT
+        settings.STATIC_URL, document_root=settings.STATIC_ROOT,
     )
